b8c2af7da134af121c8bbd90a9532ab7b8471356
Lucas Bittencourt <luksbitten@gmail.com>
2020-09-19 18:09:29 -0400

    Project Setup

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+
+    private float speed = 12f;
+    [SerializeField] private float jumpHeight = 3f;
+    [SerializeField] private float gravity = -9.8f;
+    private Vector3 velocity;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance = 0.2f;
+    [SerializeField] private LayerMask groundMask;
+    private bool isGrounded;
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        controller.Move(move * (speed * Time.deltaTime));
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position,groundDistance);
+    }
+}
---
+++ Assets/_Scripts/ShootingController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShootingController : MonoBehaviour
+{
+    public float weaponRange = 50f;                       // Distance in Unity units over which the Debug.DrawRay will be drawn
+
+    [SerializeField]private Camera fpsCam;                                // Holds a reference to the first person camera
+
+    [SerializeField] private LayerMask hitLayer;
+    [SerializeField] private Image dot;
+
+    void Update()
+    {
+        // Create a vector at the center of our camera's viewport
+        Vector3 lineOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+
+        // Draw a line in the Scene View  from the point lineOrigin in the direction of fpsCam.transform.forward * weaponRange, using the color green
+        Debug.DrawRay(lineOrigin, fpsCam.transform.forward * weaponRange, Color.green);
+        RaycastHit hit;
+
+        if (Physics.Raycast(lineOrigin, fpsCam.transform.forward, out hit, weaponRange, hitLayer))
+        {
+            dot.color = Color.green;
+        }
+        else
+        {
+            dot.color = Color.red;
+
+        }
+
+    }
+}
