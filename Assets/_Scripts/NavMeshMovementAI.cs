27989e8f9364278cee8186143345c6d0572913b2
Diogo Queiroz <diogoqueirtooz1212@gmail.com>
2020-09-24 00:06:18 -0400

    [ADD] NavMesh places, NavMesh Agent on Remy to move. Working.

---
+++ Assets/_Scripts/NavMeshMovementAI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshMovementAI : MonoBehaviour
+{
+    public Transform destination;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = destination.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
