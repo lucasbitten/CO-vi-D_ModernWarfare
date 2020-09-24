8ecf81f8991920d1461a211a95fe64680c579888
Diogo Queiroz <diogoqueirtooz1212@gmail.com>
2020-09-24 18:36:43 -0400

    [add] navmesh into park level [mod] change movement script to allow loop destinations

--- Assets/_Scripts/NavMeshMovementAI.cs
+++ Assets/_Scripts/NavMeshMovementAI.cs
@@ -6,16 +6,35 @@ using UnityEngine.AI;
 public class NavMeshMovementAI : MonoBehaviour
 {
     public Transform destination;
+    public List<Transform> destinations;
+    public int destinationIndex;
+    public int counter = 0;
+    NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = destination.position;
+        destinationIndex = 0;
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = destinations[destinationIndex].position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(transform.position, destination.position) <= 1)
+        {
+            Debug.Log("Aproximate");
+            counter++;
+            destinationIndex = counter % destinations.Count;
+            destination = destinations[destinationIndex];
+            SetNewDestination();
+        }
+        //Debug.Log($"Agent = {this.transform.position}\n" +
+        //          $"Destination = {destination.position}");
+    }
+
+    void SetNewDestination()
+    {
+        agent.destination = destination.position;
     }
 }
